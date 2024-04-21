e089eb3fc5c3596c7702c23d8ff62ece7b9fb503
karnveergill <karnveerg94@gmail.com>
2024-04-21 13:57:19 -0700

    maze class initial commit

---
+++ src/maze.h
@@ -0,0 +1,20 @@
+#ifndef MAZE_H
+#define MAZE_H
+
+#include <SDL.h>
+
+class Maze
+{
+public:
+    Maze(SDL_Window* window,
+         SDL_Renderer* renderer);
+
+    /**
+     * @brief Destructor frees up window and renderer 
+    */
+    ~Maze() {}
+
+private:
+}
+
+#endif // MAZE_H
