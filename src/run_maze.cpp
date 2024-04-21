6569e7047b40621a1584a8a67e8426e89bbc0d61
karnveergill <karnveerg94@gmail.com>
2024-04-21 16:21:59 -0700

    initial appication template

--- src/run_maze.cpp
+++ src/run_maze.cpp
@@ -1,6 +1,16 @@
 #include "maze.h"
 #include <string>
 #include "version.h"
+#include <SDL.h>
+
+///////////////////////////////////////////////////////////////////////////////
+// Constants //////////////////////////////////////////////////////////////////
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+///////////////////////////////////////////////////////////////////////////////
+// Application Entry///////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[])
 {
@@ -20,6 +30,27 @@ int main(int argc, char* argv[])
             return 0; 
         }
     }
+    printf("maze : %s\n", app_version.c_str());
+
+    // Create SDL window to display maze
+    static const std::string window_title = "Maze "  + app_version;
+    SDL_Window* window = SDL_CreateWindow(window_title.c_str(), 
+                                          SDL_WINDOWPOS_CENTERED, 
+                                          SDL_WINDOWPOS_CENTERED, 
+                                          WINDOW_WIDTH, 
+                                          WINDOW_HEIGHT, 
+                                          SDL_WINDOW_SHOWN);
+
+    // Create renderer
+    static const int USE_FIRST_AVAILABLE_RENDERING_DRIVER = -1;
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, 
+                                                USE_FIRST_AVAILABLE_RENDERING_DRIVER,
+                                                SDL_RENDERER_ACCELERATED);
+                                                
+    // Destroy SDL assets
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
